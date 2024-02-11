88e3d57ecc30602ece9441455c9dcb0bb0ac641d
Kevin Downey <downey.kevin@gmail.com>
2024-02-11 21:44:49 +0100

    Add user authentication

--- Leon/ContentView.swift
+++ Leon/ContentView.swift
@@ -7,9 +7,99 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseAuth
 
+// If user is authenticated, display main financial view. If user is not authenticated, display sign in view.
 struct ContentView: View {
-    @StateObject var viewModel = FinancialViewModel()
+    @EnvironmentObject var authViewModel: AuthViewModel
+
+    var body: some View {
+           Group {
+               if authViewModel.isAuthenticated {
+                   // User is authenticated, show the main app content
+                   MainAppView()
+               } else {
+                   // User is not authenticated, show sign-in or sign-up options
+                   SignInView()
+               }
+           }
+       }
+   }
+
+// Sign In View
+struct SignInView: View {
+    @EnvironmentObject var authViewModel: AuthViewModel
+    @State private var email = ""
+    @State private var password = ""
+
+    var body: some View {
+        VStack {
+            TextField("Email", text: $email)
+                .autocapitalization(.none)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .disabled(authViewModel.isLoading)  // Disable input while loading
+            
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .disabled(authViewModel.isLoading)  // Disable input while loading
+            
+            if authViewModel.isLoading {
+                ProgressView()  // Show loading indicator
+            } else {
+                Button("Sign In") {
+                    authViewModel.signIn(email: email, password: password)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct SignUpView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var errorMessage: String?
+
+    var body: some View {
+        VStack {
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
+            
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+            }
+            
+            Button("Sign Up") {
+                signUpUser()
+            }
+            .padding()
+        }
+        .padding()
+    }
+    
+    // Sign Up
+    
+    func signUpUser() {
+        // Assuming you have a viewModel or similar object handling authentication
+        AuthViewModel().signUp(email: email, password: password) { success, error in
+            if let error = error {
+                self.errorMessage = error.localizedDescription
+            } else if success {
+                // Handle successful sign-up, e.g., navigate to the main content of your app
+            }
+        }
+    }
+}
+
+// Main Financial Model View
+struct MainAppView: View {
+    @StateObject private var viewModel = FinancialViewModel()
     @State private var symbol: String = ""
 
     var body: some View {
--- Leon/FinancialViewModel.swift
+++ Leon/FinancialViewModel.swift
@@ -5,27 +5,83 @@
 //  Created by Kevin Downey on 1/19/24.
 //
 
+import FirebaseAuth
 import Foundation
 import Combine
 
-class FinancialViewModel: ObservableObject {
-    @Published var stockQuote: StockQuote?
-    @Published var errorMessage: String?
+//User Authentication
+class AuthViewModel: ObservableObject {
+    @Published var isAuthenticated = false
+    @Published var isLoading = false  // Add this line to track loading state
     
-    //Fetch stock quotes
-    func fetchStockQuote(forSymbol symbol: String) {
+    init() {
+        // Check the authentication state at initialization
+        checkAuthState()
+    }
+    
+    func checkAuthState() {
+        // Update isAuthenticated based on Firebase Auth state
+        isAuthenticated = Auth.auth().currentUser != nil
+    }
+    
+    //Sign In
+    func signIn(email: String, password: String) {
+        isLoading = true  // Start loading
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            DispatchQueue.main.async {
+                self?.isLoading = false  // Stop loading
+                if let error = error {
+                    // Handle error
+                    return
+                }
+                self?.isAuthenticated = true
+            }
+        }
+    }
+    // Sign Out
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            isAuthenticated = false
+        } catch let signOutError as NSError {
+            print("Error signing out: \(signOutError.localizedDescription)")
+        }
+    }
+    // Sign Up
+    func signUp(email: String, password: String, completion: @escaping (_ success: Bool, _ error: Error?) -> Void) {
+        // Implementation
+        isLoading = true  // Start loading
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+            DispatchQueue.main.async {
+                self?.isLoading = false  // Stop loading
+                if let error = error {
+                    // Handle error
+                    return
+                }
+                self?.isAuthenticated = true
+            }
+        }
+    }
+}
+    class FinancialViewModel: ObservableObject {
+        @Published var stockQuote: StockQuote?
+        @Published var errorMessage: String?
         
-           API.shared.fetchStockQuote(forSymbol: symbol) { [weak self] result in
-               DispatchQueue.main.async {
-                   switch result {
-                   case .success(let quote):
-                       print("Fetched quote: \(quote)")
-                       self?.stockQuote = quote
-                   case .failure(let error):
-                       print("Error fetching stock quote: \(error.localizedDescription)")
-                       self?.errorMessage = "Error fetching data"
-                   }
-               }
-           }
-       }
-   }
+        //Fetch stock quotes
+        func fetchStockQuote(forSymbol symbol: String) {
+            
+            API.shared.fetchStockQuote(forSymbol: symbol) { [weak self] result in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let quote):
+                        print("Fetched quote: \(quote)")
+                        self?.stockQuote = quote
+                    case .failure(let error):
+                        print("Error fetching stock quote: \(error.localizedDescription)")
+                        self?.errorMessage = "Error fetching data"
+                    }
+                }
+            }
+        }
+    }
+
